c43ded0c7442f0d7c4af6c0171d2a9671db1ce95
Kojo Addaquay <kojoaddaquay@gmail.com>
2018-08-09 15:04:41 -0400

    completed migration

---
+++ db/migrate/20180809185713_create_dose.rb
@@ -0,0 +1,9 @@
+class CreateDose < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doses do |t|
+      t.string :description
+      t.references :cocktail, foreign_key: true
+      t.references :ingredient, foreign_key: true
+    end
+  end
+end
